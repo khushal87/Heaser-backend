56d1260b76200de1c0da39d67b5648cbf43e4a89
Khushal Agarwal <khushal.agarwal987@gmail.com>
2020-11-22 12:33:16 +0530

    fix khushal87 task-time-check-fix

--- controller/Task.js
+++ controller/Task.js
@@ -17,8 +17,8 @@ exports.getEmployeeTasks = async (req, res, next) => {
                     .then(async (result) => {
                         const data = await result.filter((item) => {
                             return (
-                                moment(new Date()).format("YYYY-MM-DD") <
-                                moment(item.endDate).format("YYYY-MM-DD")
+                                moment(new Date()).format("YYYY-MM-DD hh:mm") <
+                                moment(item.endDate).format("YYYY-MM-DD hh:mm")
                             );
                         });
                         await res.status(200).json({
